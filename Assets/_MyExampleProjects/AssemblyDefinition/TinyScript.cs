ab2387ae17dac10eda3d900ce1a92ccd11a75d35
ozan kahveci <ozan.kahveci15@gmail.com>
2023-09-01 16:14:51 +0300

    Assembly Definitions

---
+++ Assets/_MyExampleProjects/AssemblyDefinition/TinyScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TinyScript : MonoBehaviour
+{
+    private void Start()
+    {
+        // DO Something 
+    }
+}   
