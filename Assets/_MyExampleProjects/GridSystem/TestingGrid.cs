7fd5ff700e7976bc2220275ecc968dedc4cf447f
ozan kahveci <ozan.kahveci15@gmail.com>
2023-09-21 19:38:37 +0300

    Add basic grid

---
+++ Assets/_MyExampleProjects/GridSystem/TestingGrid.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGrid : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid griid = new Grid(4, 2, 10f);
+    }
+
+
+
+
+}
