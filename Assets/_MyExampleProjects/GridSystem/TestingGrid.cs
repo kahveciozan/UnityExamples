1fc8fe61eacdb04bc54de28b6473b06641d83d0f
ozan kahveci <ozan.kahveci15@gmail.com>
2023-09-23 22:02:55 +0300

    Grid System

--- Assets/_MyExampleProjects/GridSystem/TestingGrid.cs
+++ Assets/_MyExampleProjects/GridSystem/TestingGrid.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class TestingGrid : MonoBehaviour
 {
+    private Grid grid;
+
     private void Start()
     {
-        Grid griid = new Grid(4, 2, 10f);
+        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+        new Grid(2, 5, 5f, new Vector3(0 ,-20));
+        new Grid(10, 10, 20f, new Vector3(-100, 30));
     }
 
 
-
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 1);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
 
 }
