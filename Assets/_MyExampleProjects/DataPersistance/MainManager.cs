87ac1aad1c716dcb680a15ade1535212be548912
ozan kahveci <47081910+kahveciozan@users.noreply.github.com>
2024-02-06 14:39:23 +0300

    Data persistance with singleton

---
+++ Assets/_MyExampleProjects/DataPersistance/MainManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void NewColorSelected(Color color)
+    {
+        MainManager.Instance.TeamColor = color;
+    }
+}
